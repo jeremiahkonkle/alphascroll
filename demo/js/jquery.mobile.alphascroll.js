7c93782ff355447cf6276a20ef4737d38573e80b
Jamieson Abbott <jamie.abbott@agilex.com>
2013-05-20 16:46:25 -0400

    Fork of AlphaScroll that uses a data-alphascroll declarative attribute extending listview so that it will be generated on the normal enhancement chain.

--- demo/js/jquery.mobile.alphascroll.js
+++ demo/js/jquery.mobile.alphascroll.js
@@ -1,129 +1,153 @@
-( function( $ ) {
+/*global jQuery */
+/*nomen: false */ (function ($, undefined) {
 
-	$.fn.extend({
+    $.widget("mobile.listview", $.mobile.listview, {
+        options: {
+            alphascroll: false
+        },
 
-		alphascroll: function() {
+        _afterListviewRefresh: function () {
+            var el = this.element;
+            this._off(el, "listviewafterrefresh");
+            this._displayList();
+            this.refresh();
+            this._on(el, {
+                listviewafterrefresh: "_afterListviewRefresh"
+            });
+        },
 
-			return this.each( function() {
+        _displayList: function () {
+            var self = this.element,
+                alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+                shortAlphabet = ['a', 'd', 'g', 'j', 'm', 'p', 's', 'w', 'z'],
+                dividers = [],
+                dividerClass, scrollbar = '';
+            //
+            // content = this.content
+            //
+            // // attach classes to list autodividers
+            $(this.element).find('.ui-li-divider').each(function () {
+                dividerClass = $(this).html().toLowerCase();
+                dividers.push(dividerClass);
+                $(this).addClass(dividerClass);
+            });
+            //
+            // // create and display the scrollbar
 
-				var content  = $( this ),
-					alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-					shortAlphabet = ['a','d','g','j','m','p','s','w','z'],
-					dividers = [],
-					dividerClass,
-					scrollbar = '';
+            function createScrollbar() {
+                var wrapper, alphascroll;
+                // generate scrollbar HTML
+                $(alphabet).each(function (index, value) {
+                    // attach the alphascroll-item class to each letter if there is a corresponding divider (acts as a link)
+                    if ($.inArray(value, dividers) > -1) {
+                        scrollbar += '<li id="alphascroll-' + value + '" class="alphascroll-item" unselectable="on">' + value.toUpperCase() + '</li>';
+                    } else {
+                        scrollbar += '<li id="alphascroll-' + value + '" unselectable="on">' + value.toUpperCase() + '</li>';
+                    }
+                });
 
-				// attach classes to list autodividers
-				$( content ).find( '.ui-li-divider' ).each( function() {
-					dividerClass = $( this ).html().toLowerCase();
-					dividers.push( dividerClass );
-					$( this ).addClass( dividerClass );
-				});
+                // attach scrollbar to page
+                $(self).wrap('<div />');
+                wrapper = self.parent();
+                $(wrapper).prepend('<ul class="alphascroll">' + scrollbar + '</ul>');
+                alphascroll = $(self).closest('div').children('.alphascroll');
 
-				// create and display the scrollbar
-				function createScrollbar() {
-					// generate scrollbar HTML
-					$( alphabet ).each( function( index, value ) {
-						// attach the alphascroll-item class to each letter if there is a corresponding divider (acts as a link)
-						if ( $.inArray( value, dividers ) > -1 ) {
-							scrollbar += '<li id="alphascroll-' + value + '" class="alphascroll-item" unselectable="on">' + value.toUpperCase() + '</li>';
-						}
-						else {
-							scrollbar += '<li id="alphascroll-' + value + '" unselectable="on">' + value.toUpperCase() + '</li>';
-						}
-					});
+                // bind touch event to scrollbar (for touch devices)
+                $(alphascroll).bind('touchmove', function (event) {
+                    event.preventDefault();
+                    var touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
+                    // scroll to divider position
+                    alphaScroll(touch.pageY);
+                });
 
-					// attach scrollbar to page
-					$( content ).wrap( '<div />' );
-					var wrapper = $( content ).parent();
-					$( wrapper ).prepend( '<ul class="alphascroll">' + scrollbar + '</ul>' );
-					var alphascroll = $( content ).closest( 'div' ).children( '.alphascroll' );
+                // bind mouse events to scrollbar (for desktop browsers)
+                $(alphascroll).bind('mousedown', function () {
+                    $('.ui-page-active').bind('mousemove', function (event) {
+                        // prevent text selection while scrolling
+                        $(this).css({
+                            "-webkit-user-select": "none",
+                            "-moz-user-select": "none",
+                            "-ms-user-select": "none",
+                            "user-select": "none"
+                        });
+                        // scroll to divider position
+                        alphaScroll(event.pageY);
+                    });
 
-					// bind touch event to scrollbar (for touch devices)
-					$( alphascroll ).bind( 'touchmove', function( event ) {
-						event.preventDefault();
-						var touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
-						// scroll to divider position
-						alphaScroll( touch.pageY );
-					});
+                    // return page to normal functioning after mouseup
+                    $('.ui-page-active').bind('mouseup', function () {
+                        // release mousemove event control
+                        $('.ui-page-active').unbind('mousemove');
+                        // return text selection to default
+                        $(this).css({
+                            "-webkit-user-select": "text",
+                            "-moz-user-select": "text",
+                            "-ms-user-select": "text",
+                            "user-select": "text"
+                        });
+                    });
+                });
 
-					// bind mouse events to scrollbar (for desktop browsers)
-					$( alphascroll ).bind( 'mousedown', function() {
-						$( '.ui-page-active' ).bind( 'mousemove', function( event ) {
-							// prevent text selection while scrolling
-							$( this ).css({
-								"-webkit-user-select" : "none",
-								"-moz-user-select"    : "none",
-								"-ms-user-select"     : "none",
-								"user-select"         : "none"
-							});
-							// scroll to divider position
-							alphaScroll( event.pageY );
-						});
+                // use short scrollbar if screen is short (like landscape on an iPhone)
+                if ($(window).height() <= 320) {
+                    truncateScrollbar();
+                }
+            }
 
-						// return page to normal functioning after mouseup
-						$( '.ui-page-active' ).bind( 'mouseup', function() {
-							// release mousemove event control
-							$( '.ui-page-active' ).unbind( 'mousemove' );
-							// return text selection to default
-							$( this ).css({
-								"-webkit-user-select" : "text",
-								"-moz-user-select"    : "text",
-								"-ms-user-select"     : "text",
-								"user-select"         : "text"
-							});
-						});
-					});
+            // handle orientation changes
+            $(window).bind('orientationchange', function () {
+                $('.alphascroll').unwrap().remove();
+                scrollbar = '';
+                createScrollbar();
+            });
 
-					// use short scrollbar if screen is short (like landscape on an iPhone)
-					if ( $( window ).height() <= 320  ) {
-						truncateScrollbar();
-					}
-				}
+            function truncateScrollbar() {
+                $('.alphascroll li').each(function (index, value) {
+                    if ($.inArray($(this).html().toLowerCase(), shortAlphabet) < 0) {
+                        $(this).html('&#183;').addClass('truncated');
+                    }
+                });
+            }
 
-				// handle orientation changes
-				$( window ).bind( 'orientationchange', function() {
-					$( '.alphascroll' ).unwrap().remove();
-					scrollbar = '';
-					createScrollbar();
-				});
+            // do the scroll
 
-				function truncateScrollbar() {
-					$( '.alphascroll li' ).each( function( index, value ) {
-						if ( $.inArray( $( this ).html().toLowerCase(), shortAlphabet ) < 0 ) {
-							$( this ).html( '&#183;' ).addClass( 'truncated' );
-						}
-					});
-				}
+            function alphaScroll(y) {
+                $('.alphascroll-item').each(function () {
 
-				// do the scroll
-				function alphaScroll( y ) {
-					$( '.alphascroll-item' ).each( function() {
-						if ( ! ( y <= $( this ).offset().top || y >= $( this ).offset().top + $( this ).outerHeight() ) ) {
-							var scroll_id = $( this ).attr( 'id' ),
-								letter    = scroll_id.split( '-' ),
-								target    = $( '.' + letter[1] ),
-								position  = target.position(),
-								header_height;
+                    if (!(y <= $(this).offset().top || y >= $(this).offset().top + $(this).outerHeight())) {
+                        var scroll_id = $(this).attr('id'),
+                            letter = scroll_id.split('-'),
+                            target = $('.' + letter[1]),
+                            position = target.position(),
+                            header_height;
+                        // offset scroll-top if header is displayed
+                        if ($('.ui-page-active [data-role="header"]').hasClass('ui-fixed-hidden')) {
+                            header_height = 0;
+                        } else {
+                            header_height = $('.ui-page-active [data-role="header"]').height();
+                        }
 
-							// offset scroll-top if header is displayed
-							if ( $( '.ui-page-active [data-role="header"]' ).hasClass( 'ui-fixed-hidden' ) ) {
-								header_height = 0;
-							}
-							else {
-								header_height = $( '.ui-page-active [data-role="header"]' ).height();
-							}
+                        // scroll the page
+                        $.mobile.silentScroll(position.top - header_height);
+                    }
+                });
+            }
 
-							// scroll the page
-							$.mobile.silentScroll( position.top - header_height );
-						}
-					});
-				}
+            // generate scrollbar on invokation
+            createScrollbar();
+        },
 
-				// generate scrollbar on invokation
-				createScrollbar();
-			});
-		}
-	});
+        _create: function () {
+            this._super();
 
-})( jQuery );
+            if (!this.options.alphascroll) {
+                return;
+            }
+
+            this._afterListviewRefresh();
+        }
+    });
+
+})(jQuery);
+
+/*nomen: true */
